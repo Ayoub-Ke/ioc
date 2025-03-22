1ad178f39407cc2417ed4941420a789316afb9cc
Ayoub-Ke <kouameayoub@gmail.com>
2025-03-22 18:58:02 +0000

    VERSION1

---
+++ src/main/java/dao/DaoImpl.java
@@ -0,0 +1,13 @@
+package dao;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+@Repository("dao")
+public class DaoImpl implements IDao {
+    @Override
+    public double getData() {
+        System.out.println("version base de données");
+        double temp = 25;
+        return temp;
+    }
+}
